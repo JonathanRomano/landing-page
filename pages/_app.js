8cfd8bbaaa153b1ae3d7484b51f1092c3c6654b2
Jonathan Lauxen Romano <jjonathanromano@gmail.com>
2022-04-04 00:22:24 -0300

    Criação do app next, implementação da estrutura basica do site e do css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body{
+          background-color: black;
+        }
+      `}
+      </style>
+    </>
+  )
 }
 
 export default MyApp
